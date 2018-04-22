70ea548139eb06cb67cf77d7c70e048ef844d63c
Valerio Bozzolan <gnu@linux.it>
2018-04-23 00:13:06 +0200

    fix null pointer and unuseful exit in Category class

--- includes/class-Category.php
+++ includes/class-Category.php
@@ -112,10 +112,9 @@ class Category {
 		];
 		if( self::$INSPECT ) {
 			print_r( $args );
-			exit;
 		}
 		if( ! self::$PORCELAIN ) {
-			return $wit->post( $args );
+			return $api->post( $args );
 		}
 	}
 
