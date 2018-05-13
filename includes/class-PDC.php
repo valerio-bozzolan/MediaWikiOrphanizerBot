2830318ee3b84352a5b9398cfefca77dbdeb897b
Valerio Bozzolan <bozzolan.valerio@educ.di.unito.it>
2018-05-13 19:36:43 +0200

    remove completly a stupid comment completly wrong. asd

--- includes/class-PDC.php
+++ includes/class-PDC.php
@@ -152,11 +152,6 @@ class PDC extends Page {
 			throw new PDCException( 'not a PDC' );
 		}
 
-		//TODO: do something when unprotected but older than 7 days
-		//if( $this->getDurationDays() > 7 ) {
-		//	throw new PDCExceptionExpired( 'not anymore a PDC of type ' . $this->getType() );
-		//}
-
 		$this->checkTitleSubjectConsistence();
 	}
 
