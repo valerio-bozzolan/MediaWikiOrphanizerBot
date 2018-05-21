66ea5f839b7a4ca7c672945aa0a9ca9394baf838
Valerio Bozzolan <valerio.bozzolan@gmail.com>
2018-05-21 15:50:08 +0200

    add an time zone configuration example

--- config-example.php
+++ config-example.php
@@ -7,6 +7,9 @@
 // Decomment the next line to inspect content before saving
 // itwikidelbot\Page::$ASK_BEFORE_SAVING = true;
 
+// Decomment and fill the next line with your time zone
+// itwikidelbot\Page::$COMMUNITY_TIMEZONE = 'Europe/Rome';
+
 // Fill the next lines with your Wikimedia bot credentials
 mw\API::$DEFAULT_USERNAME = 'Foo';
 mw\API::$DEFAULT_PASSWORD = 'Bar';
