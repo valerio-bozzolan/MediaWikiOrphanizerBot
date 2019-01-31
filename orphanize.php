6d9bf0e2f9825108114fd66a7e1205b3e38f1ac4
Daimona <38216014+Daimona@users.noreply.github.com>
2019-01-31 13:20:00 +0100

    Simplify

--- orphanize.php
+++ orphanize.php
@@ -121,9 +121,6 @@ $titles_to_be_orphanized = array_map(
 	$titles_to_be_orphanized
 );
 
-// associative array of page IDs as key and a boolean as value containg pages to be orphanized
-$involved_pageids = [];
-
 // keep a copy
 $involved_pagetitles = $titles_to_be_orphanized;
 
@@ -132,6 +129,9 @@ Log::info( 'read ' . count( $titles_to_be_orphanized ) . ' pages to be orphanize
 foreach( $titles_to_be_orphanized as $title ) {
 	Log::info( " $title" );
 }
+
+// associative array of page IDs as key and a boolean as value containg pages to be orphanized
+$involved_pageids = [];
 
 // note that the API accepts a maximum trance of titles
 while( $less_titles_to_be_orphanized = array_splice( $titles_to_be_orphanized, 0, MAX_TRANCHE_TITLES ) ) {
@@ -157,16 +157,12 @@ while( $less_titles_to_be_orphanized = array_splice( $titles_to_be_orphanized, 0
 		foreach( $response->query->pages as $page ) {
 			if( isset( $page->linkshere ) ) {
 				foreach( $page->linkshere as $linkingpage ) {
-					$pageid = (int) $linkingpage->pageid;
-					$involved_pageids[ $pageid ] = false;
+					$involved_pageids[] = (int) $linkingpage->pageid;
 				}
 			}
 		}
 	}
 }
-
-// create a clean array or page ids
-$involved_pageids = array_keys( $involved_pageids );
 
 // count of involved pages
 Log::info( sprintf(
@@ -201,81 +197,80 @@ while( $less_involved_pageids = array_splice( $involved_pageids, 0, MAX_TRANCHE_
 			$pageid = $page->pageid;
 
 			// does it have a revision?
-			if( isset( $page->revisions[ 0 ] ) ) {
+			if( !isset( $page->revisions[ 0 ] ) ) {
+				continue;
+			}
 
-				// the first revision
-				$revision = $page->revisions[ 0 ];
+			// the first revision
+			$revision = $page->revisions[ 0 ];
 
-				// timestamp of the revision useful to avoid edit conflicts
-				$timestamp = $revision->timestamp;
+			// timestamp of the revision useful to avoid edit conflicts
+			$timestamp = $revision->timestamp;
 
-				// wikitext from the main slot of this revision
-				$wikitext_raw = $revision->slots->main->{ '*' };
+			// wikitext from the main slot of this revision
+			$wikitext_raw = $revision->slots->main->{ '*' };
 
-				// create a Wikitext object
-				$wikitext = $wiki->createWikitext( $wikitext_raw );
+			// create a Wikitext object
+			$wikitext = $wiki->createWikitext( $wikitext_raw );
 
-				// for each of the titles to be orphanized
-				foreach( $involved_pagetitles as $involved_pagetitle ) {
+			// for each of the titles to be orphanized
+			foreach( $involved_pagetitles as $involved_pagetitle ) {
 
-					// parse the orphanizing title
-					$title = $wiki->createTitleParsing( $involved_pagetitle );
+				// parse the orphanizing title
+				$title = $wiki->createTitleParsing( $involved_pagetitle );
 
-					// a wikilink with and without alias
-					$wikilink_simple = $wiki->createWikilink( $title, Wikilink::NO_ALIAS );
-					$wikilink_alias  = $wiki->createWikilink( $title, Wikilink::WHATEVER_ALIAS );
+				// a wikilink with and without alias
+				$wikilink_simple = $wiki->createWikilink( $title, Wikilink::NO_ALIAS );
+				$wikilink_alias  = $wiki->createWikilink( $title, Wikilink::WHATEVER_ALIAS );
 
-					// sobstitute simple links e.g. [[Hello]]
-					$wikilink_regex_simple = $wikilink_simple->getRegex( [
-						'title-group-name' => 'title',
-					] );
+				// replace simple links e.g. [[Hello]]
+				$wikilink_regex_simple = $wikilink_simple->getRegex( [
+					'title-group-name' => 'title',
+				] );
 
-					// sobstitute links with alias e.g. [[Hello|whatever]]
-					$wikilink_regex_alias = $wikilink_alias->getRegex( [
-						'alias-group-name' => 'alias',
-					] );
+				// replace links with alias e.g. [[Hello|whatever]]
+				$wikilink_regex_alias = $wikilink_alias->getRegex( [
+					'alias-group-name' => 'alias',
+				] );
 
-					Log::debug( "regex simple wikilink:" );
-					Log::debug( $wikilink_regex_simple );
+				Log::debug( "regex simple wikilink:" );
+				Log::debug( $wikilink_regex_simple );
 
-					Log::debug( "regex wikilink aliased:" );
-					Log::debug( $wikilink_regex_alias );
+				Log::debug( "regex wikilink aliased:" );
+				Log::debug( $wikilink_regex_alias );
 
-					// convert '[[Hello]]' to 'Hello'
-					$wikitext->pregReplaceCallback( "/$wikilink_regex_simple/", function ( $matches ) {
-						return $matches[ 'title' ];
-					} );
+				// convert '[[Hello]]' to 'Hello'
+				$wikitext->pregReplaceCallback( "/$wikilink_regex_simple/", function ( $matches ) {
+					return $matches[ 'title' ];
+				} );
 
-					// convert '[[Hello|world]]' to 'world'
-					$wikitext->pregReplaceCallback( "/$wikilink_regex_alias/", function ( $matches ) {
-						return $matches[ 'alias' ];
-					} );
+				// convert '[[Hello|world]]' to 'world'
+				$wikitext->pregReplaceCallback( "/$wikilink_regex_alias/", function ( $matches ) {
+					return $matches[ 'alias' ];
+				} );
+			}
+			// end loop titles to be orphanized
+
+			// check for changes and save
+			if( $wikitext->isChanged() ) {
+				Log::info( "changes:" );
+				foreach( $wikitext->getHumanUniqueSobstitutions() as $sobstitution ) {
+					Log::info( "\t $sobstitution" );
 				}
-				// end loop titles to be orphanized
-
-				// check for changes and save
-				if( $wikitext->isChanged() ) {
-					Log::info( "changes:" );
-					foreach( $wikitext->getHumanUniqueSobstitutions() as $sobstitution ) {
-						Log::info( "\t $sobstitution" );
-					}
-					if( 'n' !== Input::yesNoQuestion( "confirm changes" ) ) {
-						$wiki->login()->edit( [
-							'pageid'    => $pageid,
-							'text'      => $wikitext->getWikitext(),
-							'summary'   => $SUMMARY,
-							'timestamp' => $timestamp,
-							'minor'     => 1,
-							'bot'       => 1,
-						] );
-					}
-					// end confirmation
-
+				if( 'n' !== Input::yesNoQuestion( "confirm changes" ) ) {
+					$wiki->login()->edit( [
+						'pageid'    => $pageid,
+						'text'      => $wikitext->getWikitext(),
+						'summary'   => $SUMMARY,
+						'timestamp' => $timestamp,
+						'minor'     => 1,
+						'bot'       => 1,
+					] );
 				}
-				// end save
+				// end confirmation
 
 			}
-			// end revision check
+			// end save
 
 		}
 		// end loop pages
