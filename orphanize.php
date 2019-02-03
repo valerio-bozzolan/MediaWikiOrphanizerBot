eb9eebe3c876cf8cd9b7145cc6b4798b93921258
Daimona <38216014+Daimona@users.noreply.github.com>
2019-02-03 15:46:13 +0100

    Skip the list page while processing, and clean it at the end (#6)

--- orphanize.php
+++ orphanize.php
@@ -119,6 +119,7 @@ Log::info( "reading $TITLE_SOURCE" );
 foreach( $responses as $response ) {
 	foreach( $response->query->pages as $page ) {
 
+		$list_pageid = $page->pageid;
 		// check warmup
 		$timestamp = reset( $page->revisions )->timestamp;
 		$timestamp = \DateTime::createFromFormat( \DateTime::ISO8601, $timestamp );
@@ -178,7 +179,9 @@ while( $less_titles_to_be_orphanized = array_splice( $titles_to_be_orphanized, 0
 		foreach( $response->query->pages as $page ) {
 			if( isset( $page->linkshere ) ) {
 				foreach( $page->linkshere as $linkingpage ) {
-					$involved_pageids[] = (int) $linkingpage->pageid;
+					if ( (int)$linkingpage->pageid !== $list_pageid ) {
+						$involved_pageids[] = (int) $linkingpage->pageid;
+					}
 				}
 			}
 		}
@@ -348,5 +351,25 @@ while( $less_involved_pageids = array_splice( $involved_pageids, 0, MAX_TRANCHE_
 
 }
 // end loop involved page IDs
+
+// Done! Remove the link from the list
+Log::info( 'removing page from the list' );
+// Fetch a fresh version of the page
+$res =
+	$wiki->createQuery( [
+		'action'  => 'query',
+		'titles'  => $TITLE_SOURCE,
+		'prop'    => 'revisions',
+		'rvslots' => 'main'
+	] );
+$content = $res->query->pages[$list_pageid]->revisions[0]->slots->main->{ '*' };
+$wikitext = $wiki->createWikitext( $content );
+foreach ( $involved_pagetitles as $title_raw ) {
+	$title = $wiki->createTitleParsing( $title_raw );
+	// Hopefully no alias has been used
+	$wlink = $wiki->createWikilink( $title, Wikilink::NO_ALIAS );
+	$reg = '!\**\s*' . $wlink->getRegex() . ',?\s*!';
+	$wikitext->pregReplace( $reg, '' );
+}
 
 Log::info( "end" );
