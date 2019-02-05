4be13a7dfd38433a817dad2bc2556df21049ca1c
Daimona <38216014+Daimona@users.noreply.github.com>
2019-02-05 10:37:25 +0100

    Manual rebase

--- orphanize.php
+++ orphanize.php
@@ -78,7 +78,7 @@ if( $opts->getArg( 'help' ) ) {
 
 // cli-only parameters
 $NO_INTERACTION = $opts->getArg( 'no-interaction' );
-$TITLE_SOURCE   = $opts->getArg( 'list', 'Utente:.avgas/Wikilink da orfanizzare' );
+$TITLE_SOURCE   = $opts->getArg( 'list', 'Utente:OrfanizzaBot/Wikilink da orfanizzare' );
 
 Log::info( "start" );
 
@@ -118,10 +118,16 @@ $responses =
 
 // collect links and take the last edit timestamp
 $titles_to_be_orphanized = [];
-
 Log::info( "reading $TITLE_SOURCE" );
 foreach( $responses as $response ) {
 	foreach( $response->query->pages as $page ) {
+
+		// check if list is unexisting
+		if( isset( $page->missing ) ) {
+			Log::error( "missing list $TITLE_SOURCE" );
+			exit( 1 );
+		}
+
 		// check warmup
 		$timestamp = reset( $page->revisions )->timestamp;
 		$timestamp = \DateTime::createFromFormat( \DateTime::ISO8601, $timestamp );
@@ -134,6 +140,7 @@ foreach( $responses as $response ) {
 		// collect links (if any)
 		if( isset( $page->links ) ) {
 			foreach( $page->links as $link ) {
+				// @TODO: does $link->title also contain the namespace itself? I think yes.
 				$titles_to_be_orphanized[] = Ns::defaultCanonicalName( $link->ns ) . $link->title;
 			}
 		}
@@ -379,8 +386,8 @@ $wikitext = $wiki->createWikitext( $content );
 // remove each entry from the list
 foreach( $involved_pagetitles as $title_raw ) {
 
-	$title = $wiki->createTitleParsing( $title_raw );
-	$wlink = $wiki->createWikilink( $title, Wikilink::WHATEVER_ALIAS )
+	$wlink = $wiki->createTitleParsing( $title_raw )
+	              ->createWikilink( Wikilink::WHATEVER_ALIAS )
 	              ->getRegex();
 
 	// strip out the whole related line and replace with something else
@@ -389,6 +396,7 @@ foreach( $involved_pagetitles as $title_raw ) {
 	// @todo In case done-text contains the full link to a page, and it has already been
 	// replaced in a previous run, don't replace it again.
 	$to = str_replace( '$1', $title_raw, $DONE_TEXT );
+
 	$wikitext->pregReplace( $from, $to );
 }
 
